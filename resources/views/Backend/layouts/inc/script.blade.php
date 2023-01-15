585697852579be727720d6a4cea40fd9f674528d
billah1 <billah9578@gmail.com>
2023-01-15 12:58:15 +0600

    sample commit

---
+++ resources/views/Backend/layouts/inc/script.blade.php
@@ -0,0 +1,27 @@
+
+    <!-- Vendor Scripts Start -->
+    <script src="{{ asset('assets/Backend') }}/js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/OverlayScrollbars.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/autoComplete.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/clamp.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/Chart.bundle.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/chartjs-plugin-rounded-bar.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/vendor/jquery.barrating.min.js"></script>
+    <!-- Vendor Scripts End -->
+
+    <!-- Template Base Scripts Start -->
+    <script src="{{ asset('assets/Backend') }}/font/CS-Line/csicons.min.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/base/helpers.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/base/globals.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/base/nav.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/base/search.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/base/settings.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/base/init.js"></script>
+    <!-- Template Base Scripts End -->
+    <!-- Page Specific Scripts Start -->
+    <script src="{{ asset('assets/Backend') }}/js/cs/charts.extend.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/pages/dashboard.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/common.js"></script>
+    <script src="{{ asset('assets/Backend') }}/js/scripts.js"></script>
+    <!-- Page Specific Scripts End -->
