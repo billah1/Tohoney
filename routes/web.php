f7d8d017589c23b37a8853e2ef715b480a7e409f
billah1 <billah9578@gmail.com>
2023-01-16 17:31:36 +0600

    admin authentication and redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('Backend.pages.dashboard');
-});
+// Route::get('/dashboard', function () {
+//     return view('Backend.pages.dashboard');
+// });
 
 Route::get('/', function () {
     return view('Frontend.pages.home');
 });
+
+// Admin Auth Route
+Route::prefix('admin/')->group(function(){
+  Route::get('login',[LoginController::class,'loginpage'])->name('admin.loginpage');
+  Route::post('login',[LoginController::class,'login'])->name('admin.login');
+  Route::get('logout',[LoginController::class,'logout'])->name('admin.logout');
+
+  Route::middleware(['auth'])->group(function(){
+    Route::get('dashboard',[DashboardController::class,'dashboard'])->name('admin.dashboard');
+  });
+
+});
+// Admin Auth Route
